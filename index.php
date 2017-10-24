b7b318bef57a4e5396164d14dfbeaece95df7b8b
EvanderVegt <e.vandervegt03@gmail.com>
2017-10-24 15:11:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$DagenWeek = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");{ 
+    for($i = 0; $i <count($DagenWeek); $i++){
+         echo $DagenWeek[$i];
+         echo"<br>";
+     }
+}  
+     
